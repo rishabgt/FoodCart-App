c8442727a751c0149c41aff98f5c739af96da718
Thilaga1299 <66373701+Thilaga1299@users.noreply.github.com>
2020-10-22 15:17:02 +0530

    created lastpage for the website

---
+++ src/app/lastpage/lastpage.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { LastpageComponent } from './lastpage.component';
+
+describe('LastpageComponent', () => {
+  let component: LastpageComponent;
+  let fixture: ComponentFixture<LastpageComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ LastpageComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(LastpageComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
