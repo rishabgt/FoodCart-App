f73ee006c5a460ea2339866899d0e5cc3bdda27a
Deepika <deepika.murugesan@mnscorp.net>
2020-10-28 14:47:05 +0530

    animations added to last page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { CartComponent } from './cart/cart.component';
 import { LoginComponent } from './login/login.component';
 import { FooterComponent } from './footer/footer.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { NgxSpinnerModule } from 'ngx-spinner';
 
 @NgModule({
   declarations: [
@@ -33,6 +34,7 @@ import { NavbarComponent } from './navbar/navbar.component';
     FormsModule,
     HttpClientModule,
     BrowserAnimationsModule,
+    NgxSpinnerModule
   ],
   providers: [DataService],
   bootstrap: [AppComponent],
--- src/app/lastpage/lastpage.component.ts
+++ src/app/lastpage/lastpage.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
   selector: 'lastpage',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LastpageComponent implements OnInit {
 
-  constructor() { }
+  constructor(private spinner: NgxSpinnerService) { }
 
   ngOnInit(): void {
+     
+    this.spinner.show();
+    setTimeout(() => {
+      /** spinner ends after 4 seconds */
+      this.spinner.hide();
+    }, 4000);
   }
 
 }
