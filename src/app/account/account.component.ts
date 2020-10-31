90b5552ce2746621cf15ddf99f7338c76e8b9b16
Delfia <lovemyself.11.jl@gmail.com>
2020-10-31 17:03:41 +0530

    modified user profile, dynamic data population in user profile

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -1,15 +1,28 @@
+import { Users } from './../models/users';
+import { DataService } from './../services/data.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'account',
   templateUrl: './account.component.html',
-  styleUrls: ['./account.component.css']
+  styleUrls: ['./account.component.css'],
 })
 export class AccountComponent implements OnInit {
+  user: Users;
+  firstName: string;
+  lastName: string;
+  userName: string;
 
-  constructor() { }
+  constructor(private service: DataService) {}
 
   ngOnInit(): void {
+    this.getUser();
   }
 
+  getUser() {
+    this.user = this.service.getUser();
+    this.firstName = this.user.firstname;
+    this.lastName = this.user.lastname;
+    this.userName = this.user.username;
+  }
 }
