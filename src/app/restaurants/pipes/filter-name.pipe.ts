6c553cee2d6d15daabcd757fe4e905ecf04facca
G.Sandhya-872894 <govindusandy99@.gmail.com>
2020-11-17 15:33:29 +0530

    changed String to string , minor chnage

--- src/app/restaurants/pipes/filter-name.pipe.ts
+++ src/app/restaurants/pipes/filter-name.pipe.ts
@@ -5,7 +5,7 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'filterName',
 })
 export class FilterNamePipe implements PipeTransform {
-  transform(value: Restaurants[], query: String): Restaurants[] {
+  transform(value: Restaurants[], query: string): Restaurants[] {
     if (query === '') {
       return value;
     } else {
