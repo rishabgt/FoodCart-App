e35938ab192343d857452e593224278c5feaebee
G.Sandhya-872894 <govindusandy99@.gmail.com>
2020-11-17 15:30:23 +0530

    Added ngx toastr messaged to submit button

--- src/app/contact-us/contact-us.component.ts
+++ src/app/contact-us/contact-us.component.ts
@@ -1,46 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-contact-us',
   templateUrl: './contact-us.component.html',
-  styleUrls: ['./contact-us.component.css']
+  styleUrls: ['./contact-us.component.css'],
 })
 export class ContactUsComponent implements OnInit {
   feedbackForm: FormGroup;
-  constructor( private formBuilder: FormBuilder, ) { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private toastr: ToastrService
+  ) {}
 
   ngOnInit(): void {
     this.feedbackForm = this.formBuilder.group({
-      name: [
-        '',
-        [
-          Validators.required,
-          Validators.pattern('^[a-zA-Z ]*$')
-        ],
-      ],
-      email: [
-        '',
-        [
-          Validators.required
-        ],
-      ],
-      message: [
-        '',
-        [
-          Validators.required,
-          Validators.maxLength(2500)
-        ],
-      ],
+      name: ['', [Validators.required, Validators.pattern('^[a-zA-Z ]*$')]],
+      email: ['', [Validators.required]],
+      message: ['', [Validators.required, Validators.maxLength(2500)]],
     });
-
   }
 
   get f() {
     return this.feedbackForm.controls;
   }
-  onSubmit() {
+  onSubmit(): void {
+    this.toastr.success('Thankyou for your valuable Feedback' + '😃');
     console.log(this.feedbackForm.value);
   }
-
 }
